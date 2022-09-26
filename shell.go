1faff8c97c2395db4a3de1adec0fc30d5dea3dee
cmetallo42 <denerohin@yandex.ru>
2022-09-26 05:18:24 +0300

    code clean up & optimizations

--- shell.go
+++ shell.go
@@ -3,13 +3,20 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 )
 
-func main() {
+const (
+	messageWelcome = `Welcome to shell-go`
+	messageHelp    = `You can type all basic linux shell commands`
+	messageExit    = `Exiting...`
+)
+
+func getDirectory() string {
 	path, err := os.Getwd()
 	if err != nil {
 		panic(err)
@@ -17,12 +24,33 @@ func main() {
 
 	splitedPath := strings.Split(path, "/")
 
-	dir := splitedPath[len(splitedPath)-1]
+	return splitedPath[len(splitedPath)-1]
+}
 
-	fmt.Print("[cmetallo@fedora " + dir + "]$ ")
+var ErrNotFound = errors.New("command not found")
+
+func internal(args []string) (err error) {
+	switch args[0] {
+	case "cd":
+		err = os.Chdir(args[1])
+	case "h", "help":
+		fmt.Println(messageHelp)
+	case "exit", "q", "quit":
+		fmt.Println(messageExit)
+		os.Exit(0)
+	default:
+		err = ErrNotFound
+	}
+
+	return
+}
+
+func main() {
+	fmt.Print(messageWelcome + "\n[cmetallo@test " + getDirectory() + "]$ ")
 
 	scanner := bufio.NewScanner(os.Stdin)
 
+scan:
 	for scanner.Scan() {
 		splited := strings.Split(scanner.Text(), " | ")
 
@@ -32,9 +60,17 @@ func main() {
 		for i := range splited {
 			args := strings.Split(splited[i], " ")
 
+			err := internal(args)
+			if err != nil && err != ErrNotFound {
+				panic(err)
+			}
+
 			cmd := exec.Command(args[0], args[1:]...)
 
-			bi.ReadFrom(&bo)
+			_, err = bi.ReadFrom(&bo)
+			if err != nil {
+				panic(err)
+			}
 			bo.Reset()
 
 			cmd.Stdin = &bi
@@ -42,14 +78,13 @@ func main() {
 			cmd.Stdout = &bo
 			cmd.Stderr = &bo
 
-			err := cmd.Run()
+			err = cmd.Run()
 			if err != nil {
-				panic(err)
+				fmt.Print(err.Error() + "\n[cmetallo@test " + getDirectory() + "]$ ")
+				continue scan
 			}
 		}
 
-		fmt.Print(bo.String())
-
-		fmt.Print("[cmetallo@fedora " + dir + "]$ ")
+		fmt.Print(bo.String() + "[cmetallo@test " + getDirectory() + "]$ ")
 	}
 }
